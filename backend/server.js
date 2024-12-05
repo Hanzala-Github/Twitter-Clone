9156554df84b48da7b68ff452417bb74a15be14c
Hanzala <han.zala010104@gmail.com>
2024-12-05 20:32:52 +0500

    second

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,8 @@ import connectMongoDB from "./db/db.js";
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // Middleware to parse body
 app.use(express.json({ limit: "10mb" }));
@@ -59,7 +61,13 @@ app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/notifications", notificationsRouter);
 
-const PORT = process.env.PORT || 5000;
+if ((process.env.NODE_ENV = process.env.NODE_ENV || "production")) {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

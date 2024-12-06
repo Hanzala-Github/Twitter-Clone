fbc93a3ab0818e4fe9b7844378a3177ef060b0f8
Hanzala <han.zala010104@gmail.com>
2024-12-06 19:43:37 +0500

    chnages

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import connectMongoDB from "./db/db.js";
-// import cookieParser from "cookie-parser";
-// // .....This is the variables part
-// dotenv.config();
-// const app = express();
-// app.use(express.json({ limit: "16kb" }));
-// app.use(express.urlencoded({ extended: true })); //To parse form data (urlencoded)
-// app.use(cookieParser());
-// const PORT = process.env.PORT || 5000;
-
-// console.log(process.env.MONGO_URI);
-// // ....This is the Routes part
-// import authRoutes from "./routes/auth.routes.js";
-// app.use("/api/auth", authRoutes);
-// // This is the userRouter
-// import userRouter from "./routes/user.routes.js";
-// app.use("/api/users", userRouter);
-// // This is the postRouter
-// import postRouter from "./routes/post.routes.js";
-// app.use("/api/posts", postRouter);
-
-// // This is the notificationsRouter
-// import notificationsRouter from "./routes/notifications.routes.js";
-// app.use("/api/notifications", notificationsRouter);
-
-// //........................ app is listening.............................//
-// app.listen(PORT, () => {
-//   console.log("Server is running on 8000 port");
-//   connectMongoDB();
-// });
-
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";

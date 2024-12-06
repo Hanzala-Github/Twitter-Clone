37ade019c63f141ba591acbb00483c6e1822b68d
Hanzala <han.zala010104@gmail.com>
2024-12-06 19:38:37 +0500

    chnages

--- frontend/jannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn.js
+++ frontend/jannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn.js
@@ -29,38 +29,6 @@
 //   }
 
 // ...........................................................//
-// import express from "express";
-// import dotenv from "dotenv";
-// import connectMongoDB from "./db/db.js";
-// import cookieParser from "cookie-parser";
-// // .....This is the variables part
-// dotenv.config();
-// const app = express();
-// app.use(express.json({ limit: "16kb" }));
-// app.use(express.urlencoded({ extended: true })); //To parse form data (urlencoded)
-// app.use(cookieParser());
-// const PORT = process.env.PORT || 5000;
-
-// console.log(process.env.MONGO_URI);
-// // ....This is the Routes part
-// import authRoutes from "./routes/auth.routes.js";
-// app.use("/api/auth", authRoutes);
-// // This is the userRouter
-// import userRouter from "./routes/user.routes.js";
-// app.use("/api/users", userRouter);
-// // This is the postRouter
-// import postRouter from "./routes/post.routes.js";
-// app.use("/api/posts", postRouter);
-
-// // This is the notificationsRouter
-// import notificationsRouter from "./routes/notifications.routes.js";
-// app.use("/api/notifications", notificationsRouter);
-
-// //........................ app is listening.............................//
-// app.listen(PORT, () => {
-//   console.log("Server is running on 8000 port");
-//   connectMongoDB();
-// });
 
 // import express from "express";
 // import dotenv from "dotenv";
@@ -104,3 +72,62 @@
 //   console.log(`Server is running on port ${PORT}`);
 //   connectMongoDB();
 // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU//
+// {
+//     "name": "twitter-clone",
+//     "version": "1.0.0",
+//     "main": "backend/server.js",
+//     "scripts": {
+//       "dev": "nodemon backend/server.js",
+//       "start": "node backend/server.js"
+//     },
+//     "keywords": [],
+//     "author": "",
+//     "type": "module",
+//     "license": "ISC",
+//     "description": "",
+//     "dependencies": {
+//       "bcryptjs": "^2.4.3",
+//       "cloudinary": "^2.5.1",
+//       "cookie-parser": "^1.4.7",
+//       "cors": "^2.8.5",
+//       "dotenv": "^16.4.5",
+//       "express": "^4.21.1",
+//       "joi": "^17.13.3",
+//       "jsonwebtoken": "^9.0.2",
+//       "mongoose": "^8.8.1",
+//       "multer": "^1.4.5-lts.1",
+//       "nodemon": "^3.1.7",
+//       "uuid": "^11.0.3"
+//     }
+//   }
+  
+  
+  
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
 // Load environment variables from .env
-dotenv.config();
+// dotenv.config();
 
 export default defineConfig({
   plugins: [react()],

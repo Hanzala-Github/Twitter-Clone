af970acb3f0313a2d555e1efbb1511f527bc2c04
Hanzala <han.zala010104@gmail.com>
2024-12-06 15:00:05 +0500

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-// https://vite.dev/config/
+// Load environment variables from .env
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_REACT_APP_BACKEND_BASEURL, // Use the environment variable
+        target: process.env.VITE_REACT_APP_BACKEND_BASEURL, // Use the variable from dotenv
         changeOrigin: true,
       },
     },
